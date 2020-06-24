460d64d8f2460472330e1a90df51d6667d07a51c
Motasem Alfarra <48854894+MotasemAlfarra@users.noreply.github.com>
2020-06-24 13:06:01 +0300

    normaliza

--- code/datasets.py
+++ code/datasets.py
@@ -49,10 +49,11 @@ def _cifar10(split: str) -> Dataset:
         return datasets.CIFAR10("./dataset_cache", train=True, download=True, transform=transforms.Compose([
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()
+            transforms.ToTensor(), transforms.Normalize(_CIFAR10_MEAN, _CIFAR10_STDDEV)
         ]))
     elif split == "test":
-        return datasets.CIFAR10("./dataset_cache", train=False, download=True, transform=transforms.ToTensor())
+        return datasets.CIFAR10("./dataset_cache", train=False, download=True, transform=transforms.Compose([transforms.ToTensor(),
+        , transforms.Normalize(_CIFAR10_MEAN, _CIFAR10_STDDEV)]) )
 
 
 def _imagenet(split: str) -> Dataset:
